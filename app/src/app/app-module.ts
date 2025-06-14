6d7efcd2c3ec68901b6bb808a5ad902106937fc1
Jorhan <jorhanalfavar@gmail.com>
2025-06-13 20:10:40 -0600

    update

--- app/src/app/app-module.ts
+++ app/src/app/app-module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CoreModule } from './core/core-module';
+import { ShareModule } from './share/share-module';
+import { HomeModule } from './home/home-module';
+import { UserModule } from './user/user-module';
+import { ProductoModule } from './producto/producto-module';
+import { OrdenModule } from './orden/orden-module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,10 +17,17 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UserModule,
+    ProductoModule,
+    OrdenModule,
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
   ],
   bootstrap: [App]
 })
