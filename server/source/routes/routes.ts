c84f4b0182e47b329fb56ff699c7d92c17d193f2
marioVMGIT <marioamurillo27@gmail.com>
2025-06-21 10:54:37 -0600

    promociones listo

--- server/source/routes/routes.ts
+++ server/source/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ResenaRoutes } from "./resena.routes";
+import { PromocionRoutes } from "./promociones.routes";
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
@@ -8,6 +9,9 @@ export class AppRoutes {
 
     // localhost:3000/resena/
     router.use("/resena", ResenaRoutes.routes);
+    
+    // localhost:3000/promocion/
+    router.use("/promocion", PromocionRoutes.routes);
 
     return router;
   }
