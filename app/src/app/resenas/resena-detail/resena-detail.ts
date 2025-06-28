497e4f9a3fab4d7e9f7cd3d1d2a47a42c599e0a5
Jorhan <jorhanalfavar@gmail.com>
2025-06-28 14:53:27 -0600

    adding Resena details

--- app/src/app/resenas/resena-detail/resena-detail.ts
+++ app/src/app/resenas/resena-detail/resena-detail.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { ResenaService } from '../../share/services/resena.service';
 
 @Component({
   selector: 'app-resena-detail',
   standalone: false,
   templateUrl: './resena-detail.html',
-  styleUrl: './resena-detail.css'
+  styleUrl: './resena-detail.css',
 })
 export class ResenaDetail {
+  datos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
+  stars = Array(5);
+  
+  constructor(
+    private ResenaService: ResenaService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) {
+    let id = this.activeRoute.snapshot.paramMap.get('id');
+    if (!isNaN(Number(id))) this.obtenerResena(Number(id));
+  }
+
+  obtenerResena(id: number) {
+    this.ResenaService.getById(id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.datos = data;
+      });
+  }
+  goback() {
+    this.router.navigate(['/resena']);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true); // Emite un valor para notificar a 'takeUntil'
+    this.destroy$.unsubscribe(); // Completa el Subject 'destroy$' para liberar recursos
+  }
 }
