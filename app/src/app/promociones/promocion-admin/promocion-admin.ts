8c7b857ee8f48ac359ca33ac780f284ccd5261b3
marioVMGIT <marioamurillo27@gmail.com>
2025-07-17 21:35:51 -0600

    promo-admin

--- app/src/app/promociones/promocion-admin/promocion-admin.ts
+++ app/src/app/promociones/promocion-admin/promocion-admin.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PromocionService } from '../../share/services/promocion.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-promocion-admin',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './promocion-admin.css'
 })
 export class PromocionAdmin {
+ promociones: any[] = [];
 
+  constructor(
+    private promocionService: PromocionService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.cargarPromociones();
+  }
+
+  cargarPromociones(): void {
+    this.promocionService.get().subscribe({
+      next: (data) => {
+        this.promociones = data;
+      },
+      error: (err) => {
+        console.error('Error al cargar promociones:', err);
+      }
+    });
+  }
+
+  irACrear(): void {
+    this.router.navigate(['/promocion/create']);
+  }
+
+  irAEditar(id: number): void {
+    this.router.navigate(['/promocion/update', id]);
+  }
 }
